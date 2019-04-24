2c1dac5dac1a02ed24d54f61c433656501c5f353
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-04-23 21:37:48 -0500

    chore: initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Theme Carbon',
+  },
+  __experimentalThemes: [
+    {
+      resolve: 'gatsby-theme-carbon',
+      options: {
+        name: 'Gatsby Theme Carbon Starter',
+        shortName: 'Carbon Starter',
+      },
+    },
+  ],
+};

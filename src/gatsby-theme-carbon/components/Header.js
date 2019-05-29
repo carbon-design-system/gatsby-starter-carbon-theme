54928994c8f04fd31feacf8edd8177f8c7693930
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-05-28 19:04:53 -0500

    chore: update

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Header from 'gatsby-theme-carbon/src/components/Header';
 
-const CustomSwitcher = () => (
-  <Header>
-    gatsby theme <span style={{ fontWeight: 200 }}>&nbsp;Carbon</span>
+const CustomSwitcher = props => (
+  <Header {...props}>
+    <span>Gatsby theme</span>&nbsp;Carbon
   </Header>
 );
 

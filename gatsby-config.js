c3e585f738f0790b4426ba9e1eef561721862be5
Vince Picone <vp@vincepic.one>
2020-11-23 14:23:14 -0600

    chore: update theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Theme Carbon',
-    description: 'A Gatsby theme for the carbon design system',
-    keywords: 'gatsby,theme,carbon',
+    title: "Gatsby Theme Carbon",
+    description: "A Gatsby theme for the carbon design system",
+    keywords: "gatsby,theme,carbon",
   },
-  pathPrefix: `/gtc`,
   plugins: [
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Carbon Design Gatsby Theme',
-        short_name: 'Gatsby Theme Carbon',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#0062ff',
-        display: 'browser',
+        name: "Carbon Design Gatsby Theme",
+        icon: "src/images/favicon.svg",
+        short_name: "Gatsby Theme Carbon",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#0062ff",
+        display: "browser",
       },
     },
-    {
-      resolve: 'gatsby-theme-carbon',
-      options: {
-        mediumAccount: 'carbondesign',
-        repository: {
-          baseUrl:
-            'https://github.com/carbon-design-system/gatsby-theme-carbon',
-          subDirectory: '/packages/example',
-        },
-      },
-    },
+    'gatsby-theme-carbon'
   ],
 };
